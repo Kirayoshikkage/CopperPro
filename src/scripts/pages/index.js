7f2ee4896c226c4c44a4a7c69fee0ce93931867b
Matviei <matviei.maksimov@bk.ru>
2024-07-01 13:43:49 +0700

    Create section about-hero (desctop)

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,3 +1,4 @@
+import common from '../common/_common.js';
 import {
   bestSalesSlider,
   newItemSlider,
@@ -5,33 +6,9 @@ import {
   discountsSlider,
   reviewsSlider,
 } from '../helpers/configuresSliders.js';
-import BurgerMenu from '../components/BurgerMenu.js';
-import FocusLock from '../components/FocusLock.js';
-import getFontSizeBody from '../helpers/getFontSizeBody.js';
 import updateVarHeaderHeightForIndexHero from '../helpers/updateVarHeaderHeightForIndexHero.js';
 
-const focusLock = new FocusLock({
-  exception: ['.burger-menu', '.header .burger-trigger'],
-  mutationObserver: true,
-  disableOnMobileDevice: true,
-});
-
-const burgerMenu = new BurgerMenu({
-  container: '.burger-menu',
-  trigger: '.header .burger-trigger',
-  body: '.burger-menu__body',
-  breakpoints: {
-    // 36rem - 576px
-    [getFontSizeBody() * 36]() {
-      if (burgerMenu.isOpen()) {
-        burgerMenu.close();
-      }
-    },
-  },
-  focusLock,
-});
-
-burgerMenu.init();
+common();
 
 updateVarHeaderHeightForIndexHero();
 
